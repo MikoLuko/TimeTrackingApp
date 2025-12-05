33f16549b4f18b62dde90fa5c26b3dbdac2f0c63
MikoLuko <mikilukomski@gmail.com>
2025-12-06 00:16:39 +0100

    Aplikacja z logowaniem i początkową autoryzacją po rolach

---
+++ TimeTrackingApp/TimeTrackingApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TimeTrackingApp.Models.Entities;
+
+namespace TimeTrackingApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TimeEntry> TimeEntries { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<EmailLog> Emaillogs { get; set; }
+
+    }
+}
---
+++ TimeTrackingApp/TimeTrackingApp/Models/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TimeTrackingApp.Models.Entities
+{
+
+        public class User: IdentityUser
+   {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Position { get; set; } = "";
+        public string Department { get; set; } = "";
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public List<TimeEntry> TimeEntries { get; set; }
+        public List<LeaveRequest> LeaveRequests { get; set; }
+    }
+
+}
